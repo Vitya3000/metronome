014837c924ad617b2fda1d0a31e5197a8bf4b7f0
Vitya3000 <109411554+Vitya3000@users.noreply.github.com>
2023-05-24 16:15:22 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,77 @@
+//Metronom (Gibt den rhytmus an)
+music.setTempo(400)
+let temp=500;
+let play = false;
+let Volume =0;
+basic.forever(function() {
+
+        if(input.buttonIsPressed(Button.B)){
+            temp-=20;
+       
+    } 
+    if (input.buttonIsPressed(Button.A)) {
+        temp += 20;
+       
+    }
+            if(input.soundLevel()<50){
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+            }
+    if (input.soundLevel() > 50 && input.soundLevel()<100) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . # . .
+        `)
+    
+    }
+    if (input.soundLevel() > 100 && input.soundLevel() < 150) {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    } if (input.soundLevel() > 150 && input.soundLevel() < 200) {
+        basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    }
+    if (input.soundLevel() > 200 && input.soundLevel() < 255) {
+        basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    }
+    if(play){
+       
+    music.playTone(Note.C3, music.beat(100))
+   
+    basic.pause(temp);
+    }
+    
+})
+
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function() {
+    if(!play){
+        play =true;
+    }
+    else{
+        play = false;
+    }
+})
